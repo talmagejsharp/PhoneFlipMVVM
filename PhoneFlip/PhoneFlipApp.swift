2ea38803e94f2fc0c8fea945e18af13cd5e8fd67
talmagesharp <talmagesharp321@gmail.com>
2024-03-12 14:02:50 -1000

    Initial Commit

---
+++ PhoneFlip/PhoneFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneFlipApp.swift
+//  PhoneFlip
+//
+//  Created by Talmage Sharp on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PhoneFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
